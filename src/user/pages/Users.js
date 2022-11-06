dcce6c48083f20e5cd2c6e9444f49f6359f2388a
Lautaro Bazzarini <lbazzarini@quadiontech.com>
2022-11-06 12:15:59 -0300

    styles and more components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,12 +3,12 @@ import UsersList from '../components/UsersList';
 
 const Users = () => {
   const USERS = [
-    // {
-    //   id: 'u1',
-    //   name: 'Pepe',
-    //   image: 'https://static.wikia.nocookie.net/hieloyfuego/images/3/3b/Balerion_the_Black_Dread_by_Lindsey_Burcar%C2%A9.png/revision/latest/scale-to-width-down/250?cb=20150107011817',
-    //   places: 3
-    // }
+    {
+      id: 'u1',
+      name: 'Pepe',
+      image: 'https://static.wikia.nocookie.net/hieloyfuego/images/3/3b/Balerion_the_Black_Dread_by_Lindsey_Burcar%C2%A9.png/revision/latest/scale-to-width-down/250?cb=20150107011817',
+      places: 3
+    }
   ];
 
   return (
