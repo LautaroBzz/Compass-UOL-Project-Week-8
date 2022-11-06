70f04e8c6dddbc838d7935fad222197a702513dc
Lautaro Bazzarini <lbazzarini@quadiontech.com>
2022-11-06 11:39:38 -0300

    UserItem component

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './UserItem.css';
 
-const UsersItem = () => {
-  return <h2>Users working!</h2>
+const UsersItem = (props) => {
+  return (
+    <li className='user-item'>
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className='user-item__info'>
+          <h2>{props.name}</h2>
+          <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+        </div>
+      </div>
+    </li>
+  )
 };
 
 export default UsersItem;
